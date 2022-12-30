ae2489579265ae1a90ef9932787098410f2aa0e9
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-12-29 23:13:37 -0500

    rf

---
+++ QuickPollTests/PollManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using FakeItEasy;
+using QuickPollLibrary.Managers;
+using QuickPollLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPollTests;
+
+public class PollManagerTests
+{
+    private PollManager _testPollManager;
+
+	public PollManagerTests()
+	{
+        _testPollManager = A.Fake<PollManager>();
+
+    }
+
+    [Fact]
+    public async void AddPoll_ShouldAddPollToAllPollsList()
+    {
+        PollModel poll = new();
+
+        await _testPollManager.AddPoll(poll);
+
+        //Assert.Contains(poll, _testPollManager.);
+    }
+
+    [Fact]
+    public async void RemovePoll_ShouldRemovePollFromAllPollsList()
+    {
+        PollModel poll = new();
+        UserModel user = new();
+
+        await _testPollManager.AddPoll(poll);
+
+        //Assert.Contains(poll, _testPollManager.AllPolls);
+
+        await _testPollManager.DeletePoll(poll, user);
+
+        //Assert.DoesNotContain(poll, _testPollManager.AllPolls);
+    }
+}
