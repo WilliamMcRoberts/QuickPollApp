74e2ae6516de0b6d8ee09676db4c38cfa4ff2d5d
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-01-01 15:51:36 -0500

    rf

--- QuickPollTests/PollManagerTests.cs
+++ QuickPollTests/PollManagerTests.cs
@@ -14,29 +14,4 @@ public class PollManagerTests
         _testPollManager = A.Fake<PollManager>();
 
     }
-
-    [Fact]
-    public async void AddPoll_ShouldAddPollToAllPollsList()
-    {
-        PollModel poll = new();
-
-        await _testPollManager.AddPoll(poll);
-
-        //Assert.Contains(poll, _testPollManager.);
-    }
-
-    [Fact]
-    public async void RemovePoll_ShouldRemovePollFromAllPollsList()
-    {
-        PollModel poll = new();
-        UserModel user = new();
-
-        await _testPollManager.AddPoll(poll);
-
-        //Assert.Contains(poll, _testPollManager.AllPolls);
-
-        await _testPollManager.DeletePoll(poll, user);
-
-        //Assert.DoesNotContain(poll, _testPollManager.AllPolls);
-    }
 }
