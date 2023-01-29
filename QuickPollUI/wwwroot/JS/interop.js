b9fea0a1728f0c9b89cce81a2271cb1ccdb28bab
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-01-28 20:13:47 -0500

    fly in animation on create and from bottom / JSInterop

--- QuickPollUI/wwwroot/JS/interop.js
+++ QuickPollUI/wwwroot/JS/interop.js
@@ -1,4 +1,28 @@
 ﻿
+window.onscroll = () => {
+    checkCard();
+}
+
+function checkCard() {
+
+    const cons = document.querySelectorAll('.con')
+
+    const triggerBottom = window.innerHeight / 5 * 5
+
+    cons.forEach(con => {
+
+        const conTop = con.getBoundingClientRect().top
+
+        if (conTop < triggerBottom) {
+            con.classList.add('fly')
+            con.classList.remove('no-fly')
+            return
+        }
+        con.classList.remove('fly')
+        con.classList.add('no-fly')
+    })
+}
+
 function clipboardCopy(text) {
     navigator.clipboard.writeText(text)
     return true;
